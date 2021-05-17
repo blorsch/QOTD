61a6ca42033e8cd9273ec70cab7cf93733b45cbe
Beck L <11636145+blorsch@users.noreply.github.com>
2021-05-16 18:55:18 -0700

    starting fe

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,14 @@ from flask import Flask, request, escape, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.expression import func
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+CORS(app)
 
 from models import Quote
 
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="app">
+    <button id="edit" class="button">Edit</button>
+    <div id="quote">
+      <h1 class="title is-1 has-text-white" style="z-index: 1; position: relative">"{{quoteText}}"</h1>
+      <h4 class="subtitle has-text-white" style="z-index: 1; position: relative">{{quoteAuthor}}</h4>
+    </div>
+    <v-vanta effect="fog" :options=vantaOptions></v-vanta>
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+import VVanta from 'vue-vanta';
+
+export default {
+  name: 'App',
+  components: {
+    VVanta
+  },
+  data: function() {
+    return {
+      quoteText: "",
+      quoteAuthor: "",
+      vantaOptions: {
+        mouseControls: false,
+        touchControls: false,
+        minHeight: 500.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00
+      }
+    };
+  },
+  mounted() {
+    const instance = this
+    axios.defaults.baseURL = 'http://localhost:5000';
+    axios.get("/random-quote").then(function (response) {
+      const quote = response.data.quotes[0]
+      instance.quoteText = quote.text
+      instance.quoteAuthor = quote.author
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #FFFFFF;
+  margin-top: 0px;
+}
+
+.vanta-container {
+  position: absolute;
+  z-index: 0;
+  top: 0px;
+  left: 0px;
+  bottom: 0px;
+  height: 100%;
+  width: 100%;
+}
+
+#quote {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  top: 0px;
+  left: 0px;
+  bottom: 0px;
+  height: 100%;
+  width: 100%;
+}
+
+#edit {
+  position: fixed;
+  right: 10px;
+  bottom: 10px;
+  height: 20px;
+  width: 50px;
+  padding: 20px 50px;
+  z-index: 1;
+}
+
+.button {
+  display: none !important;
+}
+
+.button:hover {
+  display: inline-flex !important;
+}
+</style>
